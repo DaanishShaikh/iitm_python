25286d2e518ad268aa53cb9f9e52a3c1e0d91eec
daanish <daanish222@gmail.com>
2025-01-05 21:11:01 +0530

    05-01-2025

---
+++ exponentiation_operator.py
@@ -0,0 +1,3 @@
+print("this is the exponentiation operator (**): ", 2**3) 
+#output will be 2 to the power 3 i.e. 8
+
---
+++ floor_division.py
@@ -0,0 +1,4 @@
+print("this is normal division: ",7/3)
+print("this is floor division : ",7//3)    #so it gives the largest integer before the original decimal value, 
+                                           #as if its 2.333 it will give 2
+ 
