9b273f4d1fca080c3a6d02b21aa7d3322a6b9277
daanish <daanish222@gmail.com>
2025-01-05 21:23:25 +0530

    05-01-2004

---
+++ logical_operators.py
@@ -0,0 +1,17 @@
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and false)  #notice that even thorugh i havent capitalized the first letter in false its still working, this is due to 
+#short circuit, the compilet doesnt even check the second condition as the first condition is enough to give the answer
+print(True or true)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print(not True)
+print(not False)
+
+
+
+
+
