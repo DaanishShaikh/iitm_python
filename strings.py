b96df7b4db89ffd03139402a4870bcf8e987f312
daanish <daanish222@gmail.com>
2025-01-05 22:00:16 +0530

    05-01-2025

---
+++ strings.py
@@ -0,0 +1,13 @@
+a='onomotopia'
+b='mitochondria'
+print(a+b)
+print(a[1])
+print(b[0])
+print(a[1:5])   #this will print the letters from s[1] to s[5-1]==s[4], point to be noted here is that it goes till n-1 and not n
+s='0123456789'
+x=s[7]
+y=s[8]
+print(x+y)
+c=int(s[7]) #so in python we can easily change a numeric string into an integer data type
+d=int(s[8])
+print(c+d)
