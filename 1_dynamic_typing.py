d65782bedc8ddce986d152be8422a7cd99c8c619
daanish <daanish222@gmail.com>
2025-01-06 18:29:32 +0530

    06-01-2025

---
+++ 1_dynamic_typing.py
@@ -0,0 +1,24 @@
+#here  is a demonstration on what we call dynamic typing
+#so in python, a variable's data type is dynamic, ie it can change. unlike in c++, where it wont change, it will give an error
+
+a=10
+print(type(a))
+
+a='India'
+print(type(a))
+
+a=3
+print(type(a))
+
+a=a/2
+print(type(a))
+
+a=3
+print(type(a))
+a=a/1
+print(type(a))
+
+a=10
+print(type(a))
+a=a/5
+print(type(a))
