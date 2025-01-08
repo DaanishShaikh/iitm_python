653c35bd5a62e9e36a5c1125d2d015a36afc798c
daanish <daanish222@gmail.com>
2025-01-08 10:10:32 +0530

    08-01-2024

---
+++ 5_in_operator.py
@@ -0,0 +1,4 @@
+print('alpha' in 'a variable name can only contain alpha numeric characters or underscores')
+print('alpha' in 'a variable name should start with a letter or the underscore character')
+#the in operator tells us if a particular string or character is inside a given string
+#the in operator always returns values in boolean, ie true or false
