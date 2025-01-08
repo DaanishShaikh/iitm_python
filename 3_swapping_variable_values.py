5a337b4328de0a4eeaf6f7fa35f59a6966df6582
daanish <daanish222@gmail.com>
2025-01-08 10:01:46 +0530

    08-01-2025

--- 3_swapping_variable_values.py
+++ 3_swapping_variable_values.py
@@ -1,5 +1,5 @@
 x,y=1,2
 print(x,y)
 
-x,y=y,x
+x,y=y,x       #see how simple it is to swap the values of two variables in python!
 print(x,y)
---
+++ 4_deleting_variables.py
@@ -0,0 +1,4 @@
+a=5
+print(a)
+del a
+print(a)    #note that after the deletion command, calling the a gives an error as it isnt defined anymore
