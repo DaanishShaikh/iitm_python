d56ff93553834dcff96cc860c62075fa09ee2911
daanish <daanish222@gmail.com>
2025-01-12 23:44:04 +0530

    12-01-2025

--- 6_chaining_operators.py
+++ 6_chaining_operators.py
@@ -5,4 +5,3 @@ print(10 < x < 20)
 print(x < 10 < x * 10 < 100)
 print(10 > x <= 9)
 print(5 == x > 4)
-
--- exponentiation_operator.py
+++ exponentiation_operator.py
@@ -1,3 +1,15 @@
 print("this is the exponentiation operator (**): ", 2**3) 
 #output will be 2 to the power 3 i.e. 8
 
+print(2**3**0) #example from practice assignment
+
+x = 2 ** 5
+x1 = x // 2
+x2 = x1 // 2
+x3 = x2 // 2
+x4 = x3 // 2
+x5 = x4 // 2
+print(x1 + x2 + x3 + x4 + x5)
+print(x1 x2 + x3 + x4 + x5)
+
+
